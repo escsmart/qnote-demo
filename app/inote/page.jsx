b75c0a6f88be26daedc7be200222526270d9210d
escsmart <9mkung@gmail.com>
2025-09-10 11:50:45 +0700

    commit v2

--- app/inote/page.jsx
+++ app/inote/page.jsx
@@ -6,6 +6,9 @@ import * as Icon from "react-bootstrap-icons";
 
 const inotePage = () => {
   const [isClient, setIsClient] = useState(false);
+  const [cntCheck, setCntCheck] = useState(0);
+  const [titleNote, setTitleNote] = useState("");
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState(() => {
     let result = [];
     if (typeof window !== "undefined") {
@@ -20,9 +23,14 @@ const inotePage = () => {
   useEffect(() => {
     setIsClient(true);
     localStorage.setItem("todos", JSON.stringify(todos));
+    let countChecked = 0;
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].checked == true) {
+        countChecked++;
+      }
+    }
+    setCntCheck(countChecked);
   }, [todos]);
-
-  const [todo, setTodo] = useState("");
 
   const handleTodoSubmit = () => {
     if (todo !== "") {
@@ -56,6 +64,10 @@ const inotePage = () => {
   };
 
   const handleEdit = (item) => {
+    let countChecked = 0;
+    if (item.checked == false) {
+      countChecked++;
+    }
     let newItem = {
       id: item.id,
       text: item.text,
@@ -64,7 +76,20 @@ const inotePage = () => {
     const editItem = todos.map((items) => {
       return items.id == item.id ? newItem : items;
     });
+    setCntCheck(countChecked);
     setTodos(editItem);
+  };
+
+  const handleSaveNote = async () => {
+    let arr_data = [];
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].checked == true) arr_data.push(todos[i]);
+    }
+    const payload = {
+      title: titleNote,
+      data: arr_data,
+    };
+    console.log(payload);
   };
 
   return (
@@ -73,22 +98,40 @@ const inotePage = () => {
         <div className="min-h-screen w-full bg-base-200">
           <section className="min-h-screen px-4 py-18 mb-10">
             <div className="w-full">
-              <div className="card bg-amber-100 my-2 shadow-md">
+              <div
+                className="card my-2 shadow-md relative"
+                style={{
+                  backgroundImage: "url('./bg/note-text-bg.jpg')",
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center center",
+                  backgroundSize: "cover",
+                }}
+              >
                 <textarea
                   value={todo}
                   onChange={(e) => setTodo(e.target.value)}
                   name="todo"
-                  id=""
+                  id="noteText"
                   rows="4"
-                  className="p-2"
+                  className="py-2 px-8 focus:outline-0"
                   placeholder="Enter Note heer.."
                 ></textarea>
+                <button
+                  onClick={() => {
+                    setTodo("");
+                    document.getElementById("noteText").focus();
+                  }}
+                  className={`btn btn-sm btn-circle btn-error absolute -top-2 -right-2 text-white ${
+                    todo == "" ? "hidden" : ""
+                  }`}
+                >
+                  <Icon.XLg />
+                </button>
               </div>
-              <div className="w-full flex items-center justify-between gap-3 my-4">
-                <button className="btn rounded-2xl btn-info">บันทึกร่าง</button>
+              <div className="w-full flex my-4">
                 <button
                   onClick={handleTodoSubmit}
-                  className="btn rounded-2xl btn-success text-white flex-1"
+                  className="btn rounded-2xl btn-success flex-1 text-white"
                 >
                   บันทึกข้อความ
                 </button>
@@ -96,7 +139,10 @@ const inotePage = () => {
             </div>
 
             <div className="card bg-white h-[40vh] overflow-hidden shadow-md my-4 px-2">
-              <div className="divider">รายการโน๊ต</div>
+              <div className="divider">
+                รายการโน๊ต
+                <Icon.PenFill className="text-5xl" />
+              </div>
               <div className="overflow-y-auto">
                 <table className="table table-xs table-zebra">
                   <tbody>
@@ -149,12 +195,46 @@ const inotePage = () => {
                 </table>
               </div>
             </div>
-            <button className="btn btn-lg btn-block btn-success text-white">
+            <button
+              className={`btn btn-lg btn-block btn-success text-white ${
+                cntCheck > 0 ? "" : "btn-disabled"
+              }`}
+              onClick={() => document.getElementById("my_modal_2").showModal()}
+            >
               สร้าง Note
             </button>
           </section>
         </div>
       </Template>
+
+      {/* //Modal Enter Title Note */}
+      <dialog id="my_modal_2" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">ป้อนหัวข้อสำหรับ Note นี้</h3>
+          <p className="py-4">
+            <input
+              value={titleNote}
+              onChange={(e) => setTitleNote(e.target.value)}
+              type="text"
+              className="input w-full"
+              placeholder="Enter Note Title"
+            />
+          </p>
+          <div className="w-full mt-4">
+            <form method="dialog">
+              <button
+                onClick={handleSaveNote}
+                className="btn btn-block btn-success text-white"
+              >
+                ยืนยัน
+              </button>
+            </form>
+          </div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </>
   );
 };
